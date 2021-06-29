c59ac36f69d5ba544a602b3b53557521d958045c
STIKI WEB2 <arthawebid@gmail.com>
2021-06-29 16:45:53 +0800

    Upload Perdana
    Signed-off-by: STIKI WEB2 <arthawebid@gmail.com>

--- contoh01.php
+++ contoh01.php
@@ -1,48 +1,38 @@
 <?php
     require_once 'sqlitePDO.php';
     $db = new sqlitePDO();
-    if(!$db) {
-        echo $db->lastErrorMsg();
-      }else {
-        echo "Opened database successfully\n"."<br/><br/>";
-      }
-
 
     //SQL Membuat Tabel USERS
     $sqlCreateTable ="
 		CREATE TABLE IF NOT EXISTS USERS (
 			iduser      INTEGER		PRIMARY KEY AUTOINCREMENT,
 			nama        VARCHAR(30) NOT NULL,
-			email 		VARCHAR(50) NOT NULL,
-			password  	VARCHAR(50) NOT NULL,
+			email       VARCHAR(50) NOT NULL,
+			alamat  	  VARCHAR(50),
+      telp        VARCHAR(15),    
 			date 		TIMESTAMP
 		);
 		";
     //SQL menambahkan data ke tabel USERS
     $sqlInsert ="
-		INSERT INTO USERS (nama,email,password,date) VALUES 
-        ('Wanti', 'wanti@artha.web.id', '123456', '".date("Y-m-d H:i:s")."'),
-		('Rudiantara', 'rudi@akudinal.com', '123450','".date("Y-m-d H:i:s")."');
+		INSERT INTO USERS (nama, email, alamat, telp, date) VALUES 
+        ('Wanti', 'wanti@artha.web.id', 'Jl. Mawar Merah Delima' , '081234343430', '".date("Y-m-d H:i:s")."'),
+		('Rudiantara', 'rudi@akudinal.com', 'Jl. Seroja sari Dewi' ,   '087221234504','".date("Y-m-d H:i:s")."'),
+    ('Made Artha', 'made@artha.web.id', 'Jl. Suka Dimana Saja' ,   '087221234504','".date("Y-m-d H:i:s")."');
 		";
 
     //SQL Update Tabel USERS
-    $sqlUpdate ="	
-		UPDATE USERS set password = '123456789' where email='rudi@akudinal.com';
-		";
+    $sqlUpdate ="UPDATE USERS set password = '081338457980' where email='made@artha.web.id';";
 		
-	// SQL SELECT
-	$sqlSelect ="	
-		SELECT * from USERS;
-		";
+	  // SQL SELECT
+	  $sqlSelect ="SELECT * from USERS;";
 		
-	// SQL Hapus data
-	$sqlDelete ="	
-		DELETE from USERS WHERE email='rudi@akudinal.com';
-		";
+	  // SQL Hapus data
+	  $sqlDelete ="DELETE from USERS WHERE email='rudi@akudinal.com';";
 
     
     //Membuat Tabel
-    $db->cmdSql($sqlCreateTable);
+    //$db->cmdSql($sqlCreateTable);
     
     //menambahkan data Tabel
     //$db->cmdSql($sqlInsert);
@@ -54,12 +44,16 @@
     //$db->cmdSql($sqlDelete);
 
     //Menampilkan data tabel
-    /*
+    $cx = 0;
+    echo "<h3>Data Teman</h3>";
     $hsl = $db->selectTable($sqlSelect);
     while($row = $hsl->fetch()){
+        $cx++;
+        echo "Data ke-" . $cx . "<br>";
         echo "Nama: " . $row->nama . "<br>";
         echo "Email: " . $row->email . "<br>";
-        echo "Password: ". $row->password . "<br>";
+        echo "Alamat: ". $row->alamat . "<br>";
+        echo "Telp: ". $row->telp . "<br>";
         echo "DateCreaUpdate: ". $row->date . "<br><br>";
     }
-    */
+    
